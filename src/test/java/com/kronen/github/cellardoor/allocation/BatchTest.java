53f5af5a451a2c6078d35a782c22ec776f75dbab
Kronen <kronenr@gmail.com>
2021-11-26 01:03:18 +0100

    add initial batch and order line with tests

--- src/test/java/com/kronen/github/cellardoor/allocation/BatchTest.java
+++ src/test/java/com/kronen/github/cellardoor/allocation/BatchTest.java
@@ -1,5 +1,7 @@
 package com.kronen.github.cellardoor.allocation;
 
+import org.apache.commons.lang3.tuple.MutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -11,23 +13,44 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class BatchTest {
 
     public void testAllocatingToABatchReducesTheAvailableQuantity() {
-        Batch batch = new Batch("batch-001", "SMALL-TABLE", 20, OffsetDateTime.now());
-        OrderLine line = new OrderLine("order-ref", "SMALL-TABLE", 2);
+        Pair<Batch, OrderLine> allocation = makeBatchAndLine("SMALL-TABLE",2,10);
+        Batch batch = allocation.getLeft();
+        OrderLine line = allocation.getRight();
+
         batch.allocate(line);
 
-        assertThat(batch.getAvailableQuantity()).isEqualTo(18);
+        assertThat(batch.getAvailableQuantity()).isEqualTo(8);
     }
 
     public void testCanAllocateIfAvailableGreaterThanRequired() {
-        assert false;
+        Pair<Batch, OrderLine> allocation = makeBatchAndLine("ELEGANT-LAMP",20,2);
+        Batch largeBatch = allocation.getLeft();
+        OrderLine smallLine = allocation.getRight();
+
+        assertThat(largeBatch.canAllocate(smallLine)).isTrue();
     }
 
     public void testCannotAllocateIfAvailableSmallerThanRequired() {
-        assert false;
+        Pair<Batch, OrderLine> allocation = makeBatchAndLine("ELEGANT-LAMP",2,20);
+        Batch smallBatch = allocation.getLeft();
+        OrderLine largeLine = allocation.getRight();
+
+        assertThat(smallBatch.canAllocate(largeLine)).isFalse();
     }
 
     public void testCanAllocateIfAvailableEqualToRequired() {
-        assert false;
+        Pair<Batch, OrderLine> allocation = makeBatchAndLine("ELEGANT-LAMP",2,2);
+        Batch batch = allocation.getLeft();
+        OrderLine line = allocation.getRight();
+
+        assertThat(batch.canAllocate(line)).isTrue();
+    }
+
+    public void testCannotAllocateIfSkusDoNotMatch() {
+        Batch batch = new Batch("batch-001", "UNCOMFORTABLE-CHAIR",100, null);
+        OrderLine differentSkuLine = new OrderLine("order-123", "EXPENSIVE-CHAIR", 10);
+
+        assertThat(batch.canAllocate(differentSkuLine)).isFalse();
     }
 
     public void testPrefersWarehouseBatchesToShipments() {
@@ -36,6 +59,13 @@ public class BatchTest {
 
     public void testPrefersEarlierBatches() {
         assert false;
+    }
+
+    public Pair<Batch, OrderLine> makeBatchAndLine(String sku, Integer batchQty, Integer lineQty) {
+        Batch batch = new Batch("batch-001", sku, batchQty, OffsetDateTime.now());
+        OrderLine line = new OrderLine("order-123", sku, lineQty);
+
+        return MutablePair.of(batch, line);
     }
 
 }
