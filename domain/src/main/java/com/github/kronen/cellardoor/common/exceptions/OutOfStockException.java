44e4a9e285a30401663e5086cc902813813873ad
Alberto G. Lagos <kronenr@gmail.com>
2024-11-23 17:53:03 +0100

    OutOfStockException message

--- domain/src/main/java/com/github/kronen/cellardoor/common/exceptions/OutOfStockException.java
+++ domain/src/main/java/com/github/kronen/cellardoor/common/exceptions/OutOfStockException.java
@@ -2,7 +2,7 @@ package com.github.kronen.cellardoor.common.exceptions;
 
 public class OutOfStockException extends Exception {
 
-    public static final String BATCH_UNAVAILABLE = "Could not find an available batch to allocate the sku %s";
+    public static final String BATCH_UNAVAILABLE = "Could not find an available batch to allocate the sku '%s'";
 
     public OutOfStockException(String sku) {
         super(String.format(BATCH_UNAVAILABLE, sku));
